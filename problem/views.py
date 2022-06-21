86ff9757bb930d74eca9defabb6586aca5c70bb2
GaganKaranth <gagankaranth@gmail.com>
2022-06-21 15:55:00 +0530

    modified for celery

--- problem/views.py
+++ problem/views.py
@@ -7,12 +7,16 @@ def home(request):
         rep = request.POST.get('rep')
         dur = request.POST.get('dur')
         cmd ='powershell -command '+command
-        output=''
-        for i in range(int(rep)):
-            time.sleep(int(dur))
-            p=subprocess.run(cmd,capture_output=True,text=True,shell=True)
-            output += p.stdout
-            print(output)
-        context = {'output':output}
-        return render(request,'home.html',context)
+        result=runcommand(cmd,rep,dur)
+        context = {'output':result}
+        return render(request,'home.html',context)        
     return render(request,'home.html')
+
+def runcommand(cmd,rep,dur):
+    output=''
+    for i in range(int(rep)):
+        time.sleep(int(dur))
+        p=subprocess.run(cmd,capture_output=True,text=True,shell=True)
+        output += p.stdout
+    return output
+        
